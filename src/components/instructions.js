1cd192f484729c14f61b9025d4da2c9a45228496
Sibs <sbosh11@gmail.com>
2019-03-17 20:00:52 +0200

    Update instructions.js

--- src/components/instructions.js
+++ src/components/instructions.js
@@ -4,11 +4,10 @@ import React from 'react';
 const Instructions = ({mode, time}) => {
     return (
         <div className='instr'>Instructions
-        <p>When session or break time are adjusted, start will update <br/> 
+        <p>When session or break times are adjusted, when you click start it will update <br/> 
         pause will continue the where you left off and reset will reset to default values
-  
          </p>
-        
+        <h6> Coded and designed by Sibongile Thanjekwayo</h6>
         </div>
     )
     }
